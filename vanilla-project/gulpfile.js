37926340a4034ff8c7fa90493503af336453ce9a
hades <hadesnoname0@gmail.com>
2025-03-19 23:45:08 +0300

    Creative Frontend Homework

--- vanilla-project/gulpfile.js
+++ vanilla-project/gulpfile.js
@@ -6,34 +6,35 @@ const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 const webpackStream = require('webpack-stream');
 const gulpCopy = require('gulp-copy');
-const clean = require('gulp-clean'); // Импортируем gulp-clean
+const clean = require('gulp-clean');
 
-// Задача для очистки папки ./build
 task('clean', () => {
-    return src('./build', { read: false, allowEmpty: true }) // allowEmpty позволяет не выбрасывать ошибку, если папка не существует
+    return src('./build', { read: false, allowEmpty: true }) 
         .pipe(clean());
 });
 
-// Задача для копирования публичных файлов
 task('copy-public', () => {
     return src('./src/public/**/*')
         .pipe(gulpCopy('./build', { prefix: 2 }))
         .pipe(dest('./build'));
 });
 
-// Задача для компиляции Pug
+task('copy-svg', () => {
+    return src('./src/assets/svg/**/*.svg')
+        .pipe(dest('./build/assets/svg'));
+});
+
 task('pug', () => {
     return src('./src/pug/views/**/*.pug')
         .pipe(
             pug({
-                // Ваши опции здесь
+                
             })
         )
         .pipe(dest('./build'))
         .pipe(browserSync.stream());
 });
 
-// Задача для компиляции SCSS в CSS
 task('sass', () => {
     return src('./src/style/main.scss')
         .pipe(sass().on('error', sass.logError))
@@ -41,7 +42,6 @@ task('sass', () => {
         .pipe(browserSync.stream());
 });
 
-// Задача для сборки JavaScript с помощью Webpack
 task('webpack', () => {
     return src('./src/js/main.js')
         .pipe(webpackStream(webpackConfig, webpack))
@@ -49,7 +49,6 @@ task('webpack', () => {
         .pipe(browserSync.stream());
 });
 
-// Задача для запуска сервера BrowserSync
 task('server', () => {
     browserSync.init({
         server: { baseDir: 'build/' },
@@ -58,17 +57,16 @@ task('server', () => {
     });
 });
 
-// Наблюдение за изменениями в Pug, SCSS, JS и публичных файлах
 task('watch', () => {
     watch('./src/pug/**/*.pug', series('pug'));
     watch('./src/style/**/*.scss', series('sass'));
     watch('./src/js/**/*.js', series('webpack'));
     watch('./src/public/**/*', series('copy-public'));
+    watch('./src/assets/svg/**/*.svg', series('copy-svg'));
 });
 
-// Основная задача для запуска всех задач
 task('serve', series(
-    'clean', // Очистка папки ./build перед выполнением других задач
-    parallel('pug', 'sass', 'webpack', 'copy-public'), // Параллельное выполнение задач
-    parallel('watch', 'server') // Параллельное выполнение наблюдения и сервера
+    'clean', 
+    parallel('pug', 'sass', 'webpack', 'copy-public', 'copy-svg'), 
+    parallel('watch', 'server')
 ));
