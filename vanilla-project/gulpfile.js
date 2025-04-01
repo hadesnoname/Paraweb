3f8a27a7d4ec67f254937cf00648dd847b6913b2
hades <hadesnoname0@gmail.com>
2025-04-01 23:49:31 +0300

    добавил в gulpfile.js, gulp-svg-sprite, обновил кнопки file, headline

--- vanilla-project/gulpfile.js
+++ vanilla-project/gulpfile.js
@@ -7,66 +7,76 @@ const webpackConfig = require('./webpack.config.js');
 const webpackStream = require('webpack-stream');
 const gulpCopy = require('gulp-copy');
 const clean = require('gulp-clean');
+const svgSprite = require('gulp-svg-sprite');
 
 task('clean', () => {
-    return src('./build', { read: false, allowEmpty: true }) 
-        .pipe(clean());
+  return src('./build', { read: false, allowEmpty: true })
+    .pipe(clean());
 });
 
 task('copy-public', () => {
-    return src('./src/public/**/*')
-        .pipe(gulpCopy('./build', { prefix: 2 }))
-        .pipe(dest('./build'));
+  return src('./src/public/**/*')
+    .pipe(gulpCopy('./build', { prefix: 2 }))
+    .pipe(dest('./build'));
 });
 
-task('copy-svg', () => {
-    return src('./src/assets/svg/**/*.svg')
-        .pipe(dest('./build/assets/svg'));
+task('svg-sprite', () => {
+  return src('./src/assets/svg/**/*.svg')
+    .pipe(svgSprite({
+      mode: {
+        symbol: { 
+          sprite: 'sprite.svg',
+          example: false
+        }
+      },
+      shape: {
+        id: {
+          separator: '-'
+        }
+      }
+    }))
+    .pipe(dest('./build/assets/svg'));
 });
 
 task('pug', () => {
-    return src('./src/pug/views/**/*.pug')
-        .pipe(
-            pug({
-                
-            })
-        )
-        .pipe(dest('./build'))
-        .pipe(browserSync.stream());
+  return src('./src/pug/views/**/*.pug')
+    .pipe(pug())
+    .pipe(dest('./build'))
+    .pipe(browserSync.stream());
 });
 
 task('sass', () => {
-    return src('./src/style/main.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(dest('./build/css'))
-        .pipe(browserSync.stream());
+  return src('./src/style/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./build/css'))
+    .pipe(browserSync.stream());
 });
 
 task('webpack', () => {
-    return src('./src/js/main.js')
-        .pipe(webpackStream(webpackConfig, webpack))
-        .pipe(dest('./build/js'))
-        .pipe(browserSync.stream());
+  return src('./src/js/main.js')
+    .pipe(webpackStream(webpackConfig, webpack))
+    .pipe(dest('./build/js'))
+    .pipe(browserSync.stream());
 });
 
 task('server', () => {
-    browserSync.init({
-        server: { baseDir: 'build/' },
-        notify: false,
-        online: true
-    });
+  browserSync.init({
+    server: { baseDir: 'build/' },
+    notify: false,
+    online: true,
+  });
 });
 
 task('watch', () => {
-    watch('./src/pug/**/*.pug', series('pug'));
-    watch('./src/style/**/*.scss', series('sass'));
-    watch('./src/js/**/*.js', series('webpack'));
-    watch('./src/public/**/*', series('copy-public'));
-    watch('./src/assets/svg/**/*.svg', series('copy-svg'));
+  watch('./src/pug/**/*.pug', series('pug'));
+  watch('./src/style/**/*.scss', series('sass'));
+  watch('./src/js/**/*.js', series('webpack'));
+  watch('./src/public/**/*', series('copy-public'));
+  watch('./src/assets/svg/**/*.svg', series('svg-sprite'));
 });
 
 task('serve', series(
-    'clean', 
-    parallel('pug', 'sass', 'webpack', 'copy-public', 'copy-svg'), 
-    parallel('watch', 'server')
+  'clean',
+  parallel('pug', 'sass', 'webpack', 'copy-public', 'svg-sprite'),
+  parallel('watch', 'server')
 ));
